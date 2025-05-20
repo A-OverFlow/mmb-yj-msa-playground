eb7d90bb92970fff2a792c02bf44b4f2178a076c
lucia3736 <lucia3736@naver.com>
2025-05-20 19:42:57 +0900

    fluentbit 연동

--- src/main/java/com/example/sample_yj/hello/HelloController.java
+++ src/main/java/com/example/sample_yj/hello/HelloController.java
@@ -2,11 +2,14 @@ package com.example.sample_yj.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
 
     private final HelloService helloService;
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -15,6 +18,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         HelloEntity savedEntity = helloService.saveCurrentTime();
+        log.info("🕒 [GET] /hello called");
         return "Current time: " + savedEntity.getCreatedAt();
     }
 }
